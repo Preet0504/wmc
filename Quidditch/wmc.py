52f64a55e2a79ea8e63581242ec33625153d6f86
Preet0504 <101988958+Preet0504@users.noreply.github.com>
2023-07-01 21:27:40 +0530

    Update wmc.py

--- Quidditch/wmc.py
+++ Quidditch/wmc.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import bcrypt
 
 uri = "mongodb+srv://kingpreetpatel:mongodb@cluster0.pirltzr.mongodb.net/?retryWrites=true&w=majority"
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__,template_folder="templates",static_folder="static")
 app.secret_key = 'key'
 app.static_folder = 'static'
 
@@ -18,17 +19,47 @@ try:
 except Exception as e:
     print(e)
 
+db = client['quidditch']
 @app.route("/")
 def first():
     return render_template('first.html')
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        users = db['user']
+        check_user = users.find_one({'username':request.form['username']})
+        if check_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'),check_user['password']) == check_user['password']:
+                print('User logged in successfully')
+                return redirect(url_for('home'))
+            else:
+                print('fysoab')
+        else:
+            print('fysoab')
+    print('nothing happened')
     return render_template('login.html')
 
-@app.route("/register")
+@app.route("/register",methods = ['POST','GET'])
 def register():
+    if request.method == 'POST':
+        users = db['user']
+        existing_user = users.find_one({'username':request.form['username']})
+        existing_email = users.find_one({'email':request.form['email']})
+        if existing_user is None and existing_email is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'),bcrypt.gensalt())
+            users.insert_one({'name':request.form['name'],'username':request.form['username'],'email':request.form['email'],'password':hashpass})
+            print('User registered successfully')
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
+        else:
+            print('fysoab')
+    print('nothing happened')
     return render_template('register.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
 app.debug = True
 app.run()
